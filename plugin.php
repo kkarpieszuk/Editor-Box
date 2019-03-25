64b9e956c400a8ff92a4c81b2a0b90af11c379ae
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2019-03-25 13:10:39 +0100

    added support for categories and tags

--- plugin.php
+++ plugin.php
@@ -45,8 +45,34 @@ function render_editor() {
         <textarea name="editor_box_content"
                   id="editor_box_content"
                   placeholder="<?php _e('Start writing', 'editor_box' ); ?>"></textarea>
-        <input type="submit" name="editor_box_publish" value="<?php _e("Publish", "editor_box" ); ?>">
+        <label for="editor_box_tags"><?php _e( 'Tags:', 'editor_box' ); ?></label>
+        <input type="text"
+               placeholder="<?php _e('Tags (comma seprated)', 'editor_box' ) ; ?>"
+               name="editor_box_tags"
+               class="one_third" >
+        <?php render_categories(); ?>
+        <input type="submit"
+               name="editor_box_publish"
+               value="<?php _e("Publish", "editor_box" ); ?>"
+               class="one_third">
     </form>
+<?php
+}
+
+function render_categories() {
+	$categories = get_categories( array(
+		'hide_empty' => false
+	) );
+	if ( empty( $categories ) ) return;
+    ?>
+    <label for="editor_box_categories"><?php _e( 'Category', 'editor_box' ); ?></label>
+    <select name="editor_box_categories" class="one_third">
+        <?php foreach ( $categories as $category ) : ?>
+        <option value="<?php echo $category->term_id; ?>">
+            <?php echo esc_html($category->name ); ?>
+        </option>
+        <?php endforeach; ?>
+    </select>
 <?php
 }
 
@@ -55,11 +81,20 @@ function process_post() {
          && current_user_can( "edit_posts" )
          && wp_verify_nonce( $_POST['_wpnonce'], 'editor_box_nonce' ) ) {
         if ( isset( $_POST['editor_box_title'] )  && isset( $_POST['editor_box_content'] ) ) {
-            $post_id = wp_insert_post( array(
-                    'post_content' => $_POST['editor_box_content'],
-                    'post_title' => $_POST['editor_box_title'],
-                    'post_status' => 'publish'
-            ) );
+            $post_args = array(
+	            'post_content' => $_POST['editor_box_content'],
+	            'post_title' => $_POST['editor_box_title'],
+	            'post_status' => 'publish'
+            );
+            if ( !empty( $_POST['editor_box_categories'] ) && is_numeric( $_POST['editor_box_categories'] ) ) {
+                $post_args['post_category'] = array( $_POST['editor_box_categories'] );
+            }
+	        if ( !empty( $_POST['editor_box_tags'] ) ) {
+                $tags = explode(',', $_POST['editor_box_tags'] );
+                $tags = array_map( 'esc_html', $tags );
+		        $post_args['tags_input'] = $tags;
+            }
+            $post_id = wp_insert_post( $post_args );
             $redirect_to = is_numeric( $post_id ) ? get_permalink( $post_id ) : get_site_url();
             wp_redirect( $redirect_to );
             exit();
