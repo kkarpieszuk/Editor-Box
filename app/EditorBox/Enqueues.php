006b4ea62def77cca3453ba8a72e3106cde3a6ed
konrad <kkarpieszuk@gmail.com>
2021-12-21 21:17:51 +0100

    Rewritten javascript from jQuery to vanilla-js

--- app/EditorBox/Enqueues.php
+++ app/EditorBox/Enqueues.php
@@ -5,7 +5,7 @@ namespace EditorBox;
 class Enqueues {
 	public function enqueue_editor_stuff() {
 		wp_enqueue_style( 'editor_box_style', plugins_url( 'css/editor.css', EDITORBOX_PLUGIN_FILE ));
-		wp_enqueue_script( 'editor_box_script', plugins_url( 'js/editor.js', EDITORBOX_PLUGIN_FILE ), ['jquery'] );
+		wp_enqueue_script( 'editor_box_script', plugins_url( 'js/editor.js', EDITORBOX_PLUGIN_FILE ) );
 		wp_localize_script('editor_box_script', 'editor_box_ajax', array(
 			'ajaxurl' => admin_url('admin-ajax.php')
 		));
